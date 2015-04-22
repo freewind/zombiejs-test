fd9243adfc1dbf823252d8f06ac2c77a9e75ee49
Peng.Li <peng.li@rea-group.com>
2015-04-22 14:09:48 +0800

    Add the '/delay' and `/static` routes

--- app/app.js
+++ app/app.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
+app.use('/static', express.static('static'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
+app.get('/delay', function(req, res) {
+    setTimeout(function() {
+        res.send('Delayed Hello World!')
+    }, 5000);
+});
+
+var server = app.listen(3000, function() {
 
     var host = server.address().address;
     var port = server.address().port;
