a27e822a818a2ba6de2f73bb51c14a1536ec6fcd
Peng.Li <peng.li@rea-group.com>
2015-04-25 10:08:19 +0800

    Make clear where should I put the assertion

--- test/assert-in-visit-callback-spec.js
+++ test/assert-in-visit-callback-spec.js
@@ -5,17 +5,37 @@ const assert = require('assert');
 // Which will be routed to our test server localhost:3000
 Browser.localhost('example.com', 3000);
 
+// In the homepage of http://zombie.js.org/, there is an example which does the assertions in the callback of `browser.visit`,
+// which confused me because I think we should never do it in `browser.visit`
+// In this issues, we made it clear: https://github.com/assaf/zombie/issues/903
+// the author told me that's pseudocode -_-###
+// pull request created: https://github.com/assaf/zombie/pull/905
 describe('User visits /hi', function() {
 
-    const browser = new Browser();
+    // The wrong code:
+    // it should fail the test but actually does not
+
+    // describe('assert in visit callback', function() {
+    // 	const browser = new Browser();
+    // 	it('should fail the test if the assertion failed', function(done) {
+    // 		browser.visit('/hi', function() {
+    // 			assert(browser.location.href == 'http://example.com/hi-wrong-path');
+    //          done();
+    // 		});
+    // 	});
+    // });
+
+    // The correct code:
+    // it will fail the test because the assertion is failed
 
     describe('assert in visit callback', function() {
-        it('should fail the test if the assertion failed"', function() {
-            browser.visit('/hi', function() {
-                console.log("############ location: " + browser.location.href);
-                assert(browser.location.href == 'http://example.com/hi-wrong-path');
-            });
+        const browser = new Browser();
+        before(function(done) {
+            browser.visit('/hi', done);
+        });
+
+        it('should fail the test with "done" if the assertion failed', function() {
+            assert(browser.location.href == 'http://example.com/hi-wrong-path');
         });
     });
-
 });
