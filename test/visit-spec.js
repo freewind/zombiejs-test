5e4f4aa1b8e9c7a377e12cd23a189c8a2372482e
Peng.Li <peng.li@rea-group.com>
2015-04-25 09:26:48 +0800

    browser.visit specs

--- test/visit-spec.js
+++ test/visit-spec.js
@@ -1,5 +1,6 @@
 const Browser = require('zombie');
 const moment = require('moment');
+const expect = require('expectations');
 
 // We're going to make requests to http://example.com/signup
 // Which will be routed to our test server localhost:3000
@@ -8,30 +9,81 @@ Browser.localhost('example.com', 3000);
 describe('browser.visit', function() {
 
     const browser = new Browser();
+    this.timeout(20000); // change mocha default timeout(2s)
 
-    it('should get content immediately if there is no ajax in page', function(done) {
-        const start = moment();
-        console.log('visit /static/plain.html at: ' + start);
-        browser.visit('/static/plain.html', function() {
-            const end = moment();
-            const spent = end.diff(start);
-            console.log('got content of /static/plain.html at: ' + end + ', spent: ' + spent);
+    // Note: We should never put assertion inside the callback of `browser.visit`
+    // https://github.com/assaf/zombie/issues/903
+    describe('visit a page without ajax', function() {
+        var spent = 999999;
+        before(function(done) {
+            const start = moment();
+            console.log('visit /static/plain.html at: ' + start);
+            browser.visit('/static/plain.html', function() {
+                const end = moment();
+                spent = end.diff(start);
+                // we should never put assertions inside `browser.visit`!!!
+                console.log('got content of /static/plain.html at: ' + end + ', spent: ' + spent);
+                done()
+            });
+        });
+
+        it('should get content immediately', function() {
             expect(spent).toBeLessThan(100);
-            done();
         });
     });
 
-    it('should get content immediately after ajax calls finished in a page with ajax', function(done) {
-        const start = moment();
-        console.log('visit /static/ajax.html at: ' + start);
-        browser.visit('/static/ajax.html', function() {
-            const end = moment();
-            const spent = end.diff(start);
-            console.log('got content of /static/ajax.html at: ' + end + ', spent: ' + spent);
-            // the ajax in page will get response from server in 5s
+
+    describe('the default timeout is 5s', function() {
+        var spent = 99999;
+        before(function(done) {
+            const start = moment();
+            console.log('visit /delay/9 at: ' + start);
+            browser.visit('/delay/9', function() {
+                const end = moment();
+                spent = end.diff(start);
+                console.log('got content of /delay/9 at: ' + end + ', spent: ' + spent);
+                done();
+            });
+        });
+        it("should only wait for 5s if not got the content", function() {
             expect(spent).toBeGreaterThan(5000);
             expect(spent).toBeLessThan(6000);
-            done();
-        });
+        })
     });
+
+    describe('we can change the wait time', function() {
+        var spent = 99999;
+        before(function(done) {
+            const start = moment();
+            console.log('visit /delay/9 at: ' + start);
+            browser.visit('/delay/9', {duration: '3s'}, function() {
+                const end = moment();
+                spent = end.diff(start);
+                console.log('got content of /delay/9 at: ' + end + ', spent: ' + spent);
+                done();
+            });
+        });
+        it("should only wait for 3s if not got the content", function() {
+            expect(spent).toBeGreaterThan(3000);
+            expect(spent).toBeLessThan(4000);
+        })
+    });
+
+    describe('we can use promise style', function() {
+        it("should only wait for 3s if not got the content", function(done) {
+            const start = moment();
+            console.log('visit /delay/9 at: ' + start);
+            browser.visit('/delay/1', {duration: '3s'}).then(function() {
+                const end = moment();
+                return end.diff(start);
+            }).then(function(spent) {
+                console.log("####### spent: " + spent);
+                expect(spent).toBeGreaterThan(1000);
+                expect(spent).toBeLessThan(2000);
+            }).then(done, function(err) {
+                done(err);
+            });
+        })
+    });
+
 });
